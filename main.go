813988efbe39895d0f0341d199a6594a8e5e8503
Setsu | A.K.A EthLny <60669080+Setsudan@users.noreply.github.com>
2023-01-29 14:23:32 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,61 @@
+// ---------------------------
+// FreshStart
+// ---------------------------
+// This will compile into a binary that can be run on a fresh system
+// It will read the links.txt file and open the links in the browser
+// ---------------------------
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/pkg/browser"
+)
+
+// Array of links
+var links []string
+
+func appendLink(link string) {
+	links = append(links, link)
+}
+
+func main() {
+	// Read the links.txt file
+	file, err := os.Open("links.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	// Scan the file line by line
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		appendLink(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Open the links in the browser
+	for _, link := range links {
+		// After the first link is opened wait until user input
+		if link == links[0] {
+			browser.OpenURL(link)
+			var input string
+			println("Press enter to continue")
+			fmt.Scanln(&input)
+		} else {
+			browser.OpenURL(link)
+		}
+	}
+
+	// Wait for user input
+	var input string
+	println("Press enter to exit")
+	fmt.Scanln(&input)
+}
